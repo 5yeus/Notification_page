701eee2b15e1f00d2de2ee2dd3f20aebe82c9b4d
5yeus <silas.owu@amalitech.com>
2023-06-12 11:19:36 -0700

    notification header created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,36 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
 export default {
   content: [
     '*.js',
     '*.html'
   ],
   theme: {
+    screens: {
+      'mobile': {'min':'200px','max':'427px'},
+      'tablet': {'min':'427px','max':'768px'},
+      'laptop': {'min':'770px'}      
+    },
+    fontFamily:{
+      'jakarta':['Plus Jakarta Sans','sans-serif']
+    },
+    colors:{
+      'carnation': '#f65351',
+      'cat-blue': '#0A317B',
+      'polar':'#f7fafd',
+      'white': '#fff',
+      'link-water': '#e5effa',
+      'botticelli':'#dde7ee',
+      'gull-gray': '#939dae',
+      'shuttle-gray': '#5e6778',
+      'ebony-clay':'#1c202b',
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({addVariant}){
+      addVariant("children", "&>*")
+    })
+  ],
 }
 
