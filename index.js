28bf0756b97844d5353816f71e9b5752aa84b04e
5yeus <silas.owu@amalitech.com>
2023-06-14 15:03:14 -0700

    Mark read functionality updated

--- index.js
+++ index.js
@@ -4,36 +4,48 @@ document.addEventListener('DOMContentLoaded',() =>{
     //to remove each unread-count or indicator
     const notifyCount = document.getElementById('notify-counts')
     const unreadCounts = document.querySelectorAll('.unread-counts')
+    const unreadArray = Array.from(unreadCounts)
+    const chatCounts = document.querySelectorAll('.chat')
+
     unreadCounts.forEach(unreadCount => {
         unreadCount.classList.add('cursor-pointer');
         
         unreadCount.addEventListener('click', (event) => {
             const currentTarget = event.currentTarget;
+            const chatBg = currentTarget.closest('.chat')
+            chatBg.style.backgroundColor = '#fff';
             currentTarget.remove();
-            const chatBg = document.querySelector('.chat')
-            chatBg.style.backgroundColor = '';
+
+            //update the NotifyCountValue or the UnreadArray
+            const targetIndex = unreadArray.indexOf(currentTarget)
+            if(targetIndex > -1) {
+                unreadArray.splice(targetIndex, 1);
+                updateNotifyCount();
+            }
         });
+
+        
     })
 
 
 
     const updateNotifyCount =()=>{
-        const unreadCounts = document.querySelectorAll('.unread-counts')
-        const unreadArray = Array.from(unreadCounts)
         notifyCount.textContent = unreadArray.length || 0;
     }
+
     updateNotifyCount();
     
     const markTick = document.getElementById('mark')
     markTick.addEventListener('click', () => {
         const unreadCounts = document.querySelectorAll('.unread-counts')
-        const chatCounts = document.querySelectorAll('.chat')
         chatCounts.forEach((chat)=>{
             chat.style.backgroundColor = 'white';
         })
         unreadCounts.forEach((element) => {
             element.remove();
         });
+
+        unreadArray.length = 0;
         updateNotifyCount();
     });
       
